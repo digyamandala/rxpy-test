e5eed41291a7939952ad7fb4eeb82504463d0e08
Digya Mandala Putra <41363753+digyamandala@users.noreply.github.com>
2020-06-26 22:39:08 +0700

    initial commit

---
+++ rxpy.py
@@ -0,0 +1,28 @@
+import rx
+from rx import operators as ops
+import multiprocessing
+import rx.scheduler as scheduler
+# calculate cpu count, using which will create a ThreadPoolScheduler
+thread_count = multiprocessing.cpu_count()
+thread_pool_scheduler = scheduler.ThreadPoolScheduler(thread_count)
+rx.of(1,2,3,4,5,6,7,8,9,10).pipe(
+  ops.filter(lambda i: i % 2 == 0), 
+  ops.subscribe_on(thread_pool_scheduler)
+  ).subscribe(lambda i: print(1))
+
+rx.of(1,2,3,4,5,6,7,8,9,10).pipe(
+  ops.filter(lambda i: i % 2 == 0), 
+  ops.subscribe_on(thread_pool_scheduler)
+  ).subscribe(lambda i: print(2))
+
+rx.of(1,2,3,4,5,6,7,8,9,10).pipe(
+  ops.filter(lambda i: i % 2 == 0), 
+  ops.subscribe_on(thread_pool_scheduler)
+  ).subscribe(lambda i: print(3))
+
+rx.of(1,2,3,4,5,6,7,8,9,10).pipe(
+  ops.filter(lambda i: i % 2 == 0), 
+  ops.subscribe_on(thread_pool_scheduler)
+  ).subscribe(lambda i: print(4))
+
+print("AAAAA")
